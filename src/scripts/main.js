3ec75f8906da4d74c1fb58bb2d227fc7385ac02c
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2026-02-02 21:18:02 -0300

    feat: create feature to render todos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,10 @@ const tabCompletedBtn = document.querySelector(".tablist__tab--completed");
 const clearCompletedBtn = document.querySelector(".clear-completed-btn");
 
 // Listeners
+window.addEventListener("DOMContentLoaded", () => {
+  renderTodos();
+});
+
 createTodoBtn.addEventListener("click", () => {
   console.log(isInputValueValid());
 });
@@ -49,6 +53,44 @@ function createTodo(title) {
   });
 
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function createTodoItemElement(title, isCompleted) {
+  if (typeof title !== "string") {
+    throw new TypeError("Expect a string for title property");
+  }
+
+  if (typeof isCompleted !== "boolean") {
+    throw new TypeError("Expect a boolean for isCompleted property");
+  }
+
+  if (!title.trim()) {
+    throw new Error("Expect a non-empty string for title property");
+  }
+
+  const todoItemEl = document.createElement("li");
+  const customCheckbox = document.createElement("div");
+  const label = document.createElement("label");
+
+  todoItemEl.className = "todo__item";
+
+  if (isCompleted) {
+    customCheckbox.classList.add("custom-checkbox--checked");
+    customCheckbox.innerHTML = "&check;";
+  }
+
+  customCheckbox.classList.add("custom-checkbox");
+  customCheckbox.tabIndex = 1;
+  customCheckbox.role = "checkbox";
+  customCheckbox.ariaChecked = isCompleted;
+  customCheckbox.ariaLabel = "Toggle the completeness of this todo";
+
+  label.textContent = title.trim();
+
+  todoItemEl.insertAdjacentElement("beforeend", customCheckbox);
+  todoItemEl.insertAdjacentElement("beforeend", label);
+
+  return todoItemEl;
 }
 
 /**
@@ -90,4 +132,47 @@ function getTodos() {
 function isInputValueValid() {
   const value = input.value;
   return value.trim() !== "";
+}
+
+function renderTodos() {
+  const todos = getTodos();
+
+  if (!todos.length) {
+    todoListAllEl.innerHTML =
+      "<li class='todo__item todo__item--no-todos-available'>No todos available</li>";
+    todoListActiveEl.innerHTML =
+      "<li class='todo__item todo__item--no-todos-available'>No todos available</li>";
+    todoListCompletedEl.innerHTML =
+      "<li class='todo__item todo__item--no-todos-available'>No todos available</li>";
+
+    return;
+  }
+
+  const activeTodos = todos.filter((item) => !item.isCompleted);
+  const completedTodos = todos.filter((item) => item.isCompleted);
+
+  todoListAllEl.innerText = "";
+  todoListActiveEl.innerText = "";
+  todoListCompletedEl.innerText = "";
+
+  const fillTabPanelWithTodos = (tabPanel, todosArr) => {
+    if (!todosArr.length) {
+      tabPanel.innerHTML =
+        "<li class='todo__item todo__item--no-todos-available'>No todos available</li>";
+
+      return;
+    }
+
+    todosArr.forEach((item) => {
+      const { title, isCompleted } = item;
+
+      const todoItem = createTodoItemElement(title, isCompleted);
+
+      tabPanel.insertAdjacentElement("beforeend", todoItem);
+    });
+  };
+
+  fillTabPanelWithTodos(todoListAllEl, todos);
+  fillTabPanelWithTodos(todoListActiveEl, activeTodos);
+  fillTabPanelWithTodos(todoListCompletedEl, completedTodos);
 }
